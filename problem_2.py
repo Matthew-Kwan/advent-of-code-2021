5dc7e4fc4505ef79aded641866a38e5cbc3efb11
Matthew Kwan <matthewky.kwan@gmail.com>
2021-12-05 11:03:26 -0500

    Finish prob 2

---
+++ problem_2.py
@@ -0,0 +1,41 @@
+"""
+Advent of Code 2021
+Problem 2: Dive!
+
+Input: List of Integers - depths
+Output: Integer - Number of positive increases in depth
+"""
+
+# part 1 and 2 (removed depth changes from part 1)
+def submarinePosition():
+    """
+    Returns the depth * the horizontal position of t
+    """
+    
+    # Load in data
+    with open("./inputs/prob2.txt") as fh:
+        instructions = [x.strip() for x in fh]
+        
+    depth = 0
+    x_pos = 0
+    aim = 0
+    for instruct in instructions:
+        di, val = instruct.split()
+        val = int(val)
+        
+        if di == 'forward':
+            x_pos += val
+            depth += (aim * val)
+            
+        elif di == 'up':
+            aim -= val
+            
+        else:
+            aim += val
+
+    return depth * x_pos
+
+# Test Cases: part 1
+ans = submarinePosition()
+print(ans)
+    
